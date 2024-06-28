f747b9b3cafa28cadf168006e7f5aa1c6575633f
BoonyasithKho <72428950+BoonyasithKho@users.noreply.github.com>
2024-06-28 13:28:42 +0700

    Update calculator.test.js

--- calculator.test.js
+++ calculator.test.js
@@ -1,15 +1,15 @@
-const {calculate} = require("./calculator");
+const {calculate,sum,multiple,divide} = require("./calculator");
 
 test("Adds 1+2 to additionResult 3", () => {
-  expect(sum(1, 2)).toBe(additionResult);
+  expect(sum(1, 2)).toBe(3);
 });
 
 
 test("multiply additionResult*2 to equal 2", () => {
-  expect(multiply(additionResult, 2)).toBe(multiplicationResult);
+  expect(multiply(3, 2)).toBe(6);
 });
 
 
 test("divide multiplicationResult/2 to equal 1", () => {
-  expect(divide(multiplicationResult, 2)).toBe(divide);
+  expect(divide(6, 2)).toBe(3);
 });
