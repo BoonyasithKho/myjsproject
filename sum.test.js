6aa0747037ad09bc2c0bc4d26b7762849f41ed7d
BoonyasithKho <72428950+BoonyasithKho@users.noreply.github.com>
2024-06-28 12:04:40 +0700

    Update sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,13 @@ const sum = require("./sum");
 test("Adds 1+2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+
+test("Adds 10+20 to equal 30", () => {
+  expect(sum(10, 20)).toBe(30);
+});
+
+
+test("Adds 21+22 to equal 43", () => {
+  expect(sum(21, 22)).toBe(43);
+});
